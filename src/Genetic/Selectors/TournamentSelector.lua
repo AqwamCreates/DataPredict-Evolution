a6494ea419fd36086d17a436b9929538e1e17d87
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2026-07-15 17:26:53 +0800

    Create TournamentSelector.lua

---
+++ src/Genetic/Selectors/TournamentSelector.lua
@@ -0,0 +1,88 @@
+--[[
+
+	--------------------------------------------------------------------
+
+	Aqwam's Evolution Library (DataPredict Evolution)
+
+	Author: Aqwam Harish Aiman
+	
+	Email: aqwam.harish.aiman@gmail.com
+	
+	YouTube: https://www.youtube.com/channel/UCUrwoxv5dufEmbGsxyEUPZw
+	
+	LinkedIn: https://www.linkedin.com/in/aqwam-harish-aiman/
+	
+	--------------------------------------------------------------------
+		
+	By using this library, you agree to comply with our Terms and Conditions in the link below:
+	
+	https://github.com/AqwamCreates/DataPredict-Genetics/blob/main/docs/TermsAndConditions.md
+	
+	--------------------------------------------------------------------
+	
+	DO NOT REMOVE THIS TEXT!
+	
+	--------------------------------------------------------------------
+
+--]]
+
+local BaseSelector = require(script.Parent.BaseSelector)
+
+local mathRandom = math.random
+
+local TournamentSelector = {}
+
+TournamentSelector.__index = TournamentSelector
+
+TournamentSelector.__index = TournamentSelector
+
+function TournamentSelector.new(parameterDictionary)
+	
+	parameterDictionary = parameterDictionary or {}
+	
+	local tournamentSize = parameterDictionary.tournamentSize or 2
+	
+	parameterDictionary.type = "Tournament"
+	
+	local NewTournamentSelector = BaseSelector.new(parameterDictionary)
+	
+	NewTournamentSelector.tournamentSize = tournamentSize
+	
+	return NewTournamentSelector
+	
+end
+
+function TournamentSelector:select(ChromosomeAndScoreDictionaryDictionary)
+	
+	local tournamentSize = self.tournamentSize
+	
+	local numberOfChromosomes = #ChromosomeAndScoreDictionaryDictionary
+	
+	local BestChromosome = nil
+	
+	local bestChromosomeScore = -math.huge
+
+	for i = 1, tournamentSize, 1 do
+		
+		local chromosomeIndex = mathRandom(1, numberOfChromosomes)
+
+		local chromosomeDictionary = ChromosomeAndScoreDictionaryDictionary[chromosomeIndex]
+		
+		local Chromosome = chromosomeDictionary.Chromosome
+		
+		local chromosomeScore = chromosomeDictionary.score
+		
+		if (chromosomeScore > bestChromosomeScore) then
+			
+			bestChromosomeScore = chromosomeScore
+			
+			BestChromosome = Chromosome
+			
+		end
+		
+	end
+	
+	return BestChromosome
+end
+
+return TournamentSelector
