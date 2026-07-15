593fff280197677ac474a167ff02613102708818
Aqwam Harish Aiman <67371914+AqwamCreates@users.noreply.github.com>
2026-07-15 18:12:52 +0800

    Create RankSelector.lua

---
+++ src/Genetic/Selectors/RankSelector.lua
@@ -0,0 +1,117 @@
+--[[
+
+	--------------------------------------------------------------------
+
+	Aqwam's Evolution Library (DataPredict Evolution)
+
+	Author: Aqwam Harish Aiman
+	
+	Email: aqwam.harish.aiman@gmail.com
+	
+	YouTube: https://www.youtube.com/channel/UCUrwoxv5dufEmbGsxyEUPZw
+	
+	LinkedIn: https://www.linkedin.com/in/aqwam-harish-aiman/
+	
+	--------------------------------------------------------------------
+		
+	By using this library, you agree to comply with our Terms and Conditions in the link below:
+	
+	https://github.com/AqwamCreates/DataPredict-Evolution/blob/main/docs/TermsAndConditions.md
+	
+	--------------------------------------------------------------------
+	
+	DO NOT REMOVE THIS TEXT!
+	
+	--------------------------------------------------------------------
+
+--]]
+
+local BaseSelector = require(script.Parent.BaseSelector)
+
+local mathRandom = math.random
+
+local mathPow = math.pow
+
+local RankSelector = {}
+
+RankSelector.__index = RankSelector
+
+setmetatable(RankSelector, BaseSelector)
+
+function RankSelector.new(parameterDictionary)
+	
+	parameterDictionary = parameterDictionary or {}
+	
+	local pressure = parameterDictionary.pressure or 1
+	
+	parameterDictionary.type = "Rank"
+	
+	local NewRankSelector = BaseSelector.new(parameterDictionary)
+	
+	setmetatable(NewRankSelector, RankSelector)
+	
+	NewRankSelector.pressure = pressure
+	
+	return NewRankSelector
+	
+end
+
+function RankSelector:select(ChromosomeAndScoreDictionaryArray)
+	
+	local p = self.pressure
+	
+	local numberOfChromosomes = #ChromosomeAndScoreDictionaryArray
+	
+	local totalWeight = 0
+	
+	local weightArray = {}
+
+	for arrayIndex, ChromosomeAndScoreDictionary in ipairs(ChromosomeAndScoreDictionaryArray) do
+
+		local rankValue = numberOfChromosomes - arrayIndex + 1
+
+		local weight = mathPow(rankValue, p)
+
+		weightArray[arrayIndex] = weight
+		
+		totalWeight = totalWeight + weight
+
+	end
+
+	if (totalWeight == 0) then
+		
+		local randomArrayIndex = mathRandom(1, numberOfChromosomes)
+		
+		local ChromosomeAndScoreDictionary = ChromosomeAndScoreDictionaryArray[randomArrayIndex]
+		
+		local Chromosome = ChromosomeAndScoreDictionary.Chromosome
+		
+		return Chromosome
+		
+	end
+
+	local spinValue = mathRandom() * totalWeight
+	
+	local currentSumValue = 0
+
+	for arrayIndex, ChromosomeAndScoreDictionary in ipairs(ChromosomeAndScoreDictionaryArray) do
+
+		currentSumValue = currentSumValue + weightArray[arrayIndex]
+
+		if (spinValue <= currentSumValue) then
+
+			return ChromosomeAndScoreDictionary.Chromosome
+
+		end
+
+	end
+	
+	local ChromosomeAndScoreDictionary = ChromosomeAndScoreDictionaryArray[numberOfChromosomes]
+	
+	local Chromosome = ChromosomeAndScoreDictionary.Chromosome
+	
+	return Chromosome
+	
+end
+
+return RankSelector
